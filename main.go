5a94fbd6a68acd441858b6dff4c81480e90ba2f7
TempestDrift <bridget.reese@lfest.org>
2019-03-12 14:40:55 -0700

    Added Message server owner

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 var (
 	commandPrefix string
 	botID         string
+	botname       string
 ) //setting variables? setting variable type?
 
 func main() { // calling the function command, ????
@@ -17,7 +18,8 @@ func main() { // calling the function command, ????
 	user, err := discord.User("@me")                //grabbing account information
 	errCheck("error retrieving account", err)       //check if error occurred
 
-	botID = user.ID                    //botID is a variable set to the bots information
+	botID = user.ID //botID is a variable set to the bots information
+	botname = "TestBot"
 	discord.AddHandler(commandHandler) // a listener that when it picks up a message create it runs the function
 	discord.AddHandler(func(discord *discordgo.Session, ready *discordgo.Ready) {
 		err = discord.UpdateStatus(0, "Insaniquarium!")
@@ -30,20 +32,39 @@ func main() { // calling the function command, ????
 
 	err = discord.Open()
 	errCheck("Error opening connection to Discord", err)
-	//bullshit lane
+	//bullshit lane Functional
 	for i := 0; i < len(discord.State.Guilds); i++ {
 		currentguild := discord.State.Guilds[i]
 		println(currentguild.ID)
 		channellist, err := discord.GuildChannels(currentguild.ID)
 		errCheck("error retrieving channellist", err)
 		println(channellist)
+		var (
+			messagechannel string
+		)
 		for a := 0; a < len(channellist); a++ {
+			if channellist[a].Type == discordgo.ChannelTypeGuildText {
+				messagechannel = channellist[a].ID
+			}
 			if channellist[a].Name == "general" {
 				message, err := discord.ChannelMessageSend(channellist[a].ID, "Hello Fish!")
 				errCheck("error sending message", err)
 				println(message.ID)
 			}
 		}
+		for r := 0; r < len(currentguild.Roles); r++ {
+			currentrole := currentguild.Roles[r]
+			println(currentrole.Name)
+			println(currentrole.ID)
+			if currentrole.Name == "admin" {
+				println(messagechannel)
+			}
+		}
+		ownerchannel, err := discord.UserChannelCreate(currentguild.OwnerID)
+		errCheck("error creating channel", err)
+		message, err := discord.ChannelMessageSend(ownerchannel.ID, "Welcome to the LANFest Discord bot! You are registered as the owner of "+currentguild.Name+", so you will need to answer a few questions to complete setup. If you’d rather not, please type the discord ID of another admin-level access user (e.g. "+botname+botID+") to complete setup for you. Type y to continue.")
+		errCheck("error sending message", err)
+		println(message.ID)
 	}
 
 	defer discord.Close()
